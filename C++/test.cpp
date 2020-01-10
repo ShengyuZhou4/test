a18a960ccaf4e88ce0ac3fe6fbda3a24fccbd511
sheng <shengyu2006@hotmail.com>
2020-01-10 19:38:29 +0800

    apply changes

--- C++/test.cpp
+++ C++/test.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello World!\n";
+    int n,m,t,sum,ans=1;
+    cin>>n>>m;
+    for(int i=0;i<n;++i)
+    {
+        cin>>t;
+        if(sum+t>m)
+        {
+            ++ans;
+            sum=t;
+        }
+        else
+            sum+=t;
+    }
+    cout<<ans<<'\n';
     return 0;
 }
--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,1 @@
-print("Hello World!")
+print(int(input())+int(input()))
