f259e96d8df733f86d67bb42039c24bc02f742d5
sheng <shengyu2006@hotmail.com>
2020-01-17 17:05:55 +0800

    apply changes

--- C++/test.cpp
+++ C++/test.cpp
@@ -2,19 +2,5 @@
 using namespace std;
 int main()
 {
-    int n,m,t,sum,ans=1;
-    cin>>n>>m;
-    for(int i=0;i<n;++i)
-    {
-        cin>>t;
-        if(sum+t>m)
-        {
-            ++ans;
-            sum=t;
-        }
-        else
-            sum+=t;
-    }
-    cout<<ans<<'\n';
     return 0;
 }
