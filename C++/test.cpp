12c0b2908f22db50947835c351e4fd3e75726371
sheng <shengyu2006@hotmail.com>
2020-01-26 15:10:56 +0800

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main()
 {
+    int n,x,y;
+    cin>>n;
+    vector<int> a(n+1),b(n+1),g(n+1),k(n+1);
+    for(int i=1;i<=n;++i)
+        cin>>a[i]>>b[i]>>g[i]>>k[i];
+    cin>>x>>y;
+    for(int i=n;i>=1;--i)
+        if(a[i]<=x&&x<a[i]+g[i]&&b[i]<=y&&y<b[i]+k[i])
+        {
+            cout<<i<<'\n';
+            return 0;
+        }
+    cout<<"-1\n";
     return 0;
 }
