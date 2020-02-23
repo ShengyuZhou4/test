2c33ddef3570fadc37ba6954ba0e9b3fe085caec
sheng <shengyu2006@hotmail.com>
2020-02-23 11:07:20 +0800

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,72 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
-int m,n,d;
-vector<vector<char> > v;
-void solve()
+int m,n;
+vector<vector<int> > v,l;
+void solve(int x,int y,int coin,bool magic)
 {
-    queue<pair<pair<int,int>,pair<int,int> > > q;
-    q.push(make_pair(make_pair(0,d),make_pair(1,1)));
-    vector<vector<int> > s(m+2,vector<int>(n+2,-1));
-    s[1][1]=d;
-    while(!q.empty())
-    {
-        pair<pair<int,int>,pair<int,int> > p=q.front();
-        if(p.second.first==m&&p.second.second==n)
+    if(coin>=l[x][y]||x==0||y==0||x==m+1||y==m+1)
+        return;
+    l[x][y]=coin;
+    if(x==m&&y==m)
+        return;
+    if(v[x][y]==v[x-1][y])
+        solve(x-1,y,coin,true);
+    else
+        if(v[x-1][y]!=2)
+            solve(x-1,y,coin+1,true);
+        else if(magic)
         {
-            cout<<p.first.first<<'\n';
-            return;
+            int t=v[x-1][y];
+            v[x-1][y]=v[x][y];
+            solve(x-1,y,coin+2,false);
+            v[x-1][y]=t;
         }
-        for(int i=1;i<=max(p.first.second,1);++i)
+    if(v[x][y]==v[x+1][y])
+        solve(x+1,y,coin,true);
+    else
+        if(v[x+1][y]!=2)
+            solve(x+1,y,coin+1,true);
+        else if(magic)
         {
-            if(p.second.first-i==0)
-                break;
-            if(v[p.second.first-i][p.second.second]=='P'&&p.first.second-(i==1?0:i)>s[p.second.first-i][p.second.second])
-            {
-                s[p.second.first-i][p.second.second]=p.first.second-(i==1?0:i);
-                q.push(make_pair(make_pair(p.first.first+1,p.first.second-(i==1?0:i)),make_pair(p.second.first-i,p.second.second)));
-            }
+            int t=v[x+1][y];
+            v[x+1][y]=v[x][y];
+            solve(x+1,y,coin+2,false);
+            v[x+1][y]=t;
         }
-        for(int i=1;i<=max(p.first.second,1);++i)
+    if(v[x][y]==v[x][y-1])
+        solve(x,y-1,coin,true);
+    else
+        if(v[x][y-1]!=2)
+            solve(x,y-1,coin+1,true);
+        else if(magic)
         {
-            if(p.second.first+i==m+1)
-                break;
-            if(v[p.second.first+i][p.second.second]=='P'&&p.first.second-(i==1?0:i)>s[p.second.first+i][p.second.second])
-            {
-                s[p.second.first+i][p.second.second]=p.first.second-(i==1?0:i);
-                q.push(make_pair(make_pair(p.first.first+1,p.first.second-(i==1?0:i)),make_pair(p.second.first+i,p.second.second)));
-            }
+            int t=v[x][y-1];
+            v[x][y-1]=v[x][y];
+            solve(x,y-1,coin+2,false);
+            v[x][y-1]=t;
         }
-        for(int i=1;i<=max(p.first.second,1);++i)
+    if(v[x][y]==v[x][y+1])
+        solve(x,y+1,coin,true);
+    else
+        if(v[x][y+1]!=2)
+            solve(x,y+1,coin+1,true);
+        else if(magic)
         {
-            if(p.second.second-i==0)
-                break;
-            if(v[p.second.first][p.second.second-i]=='P'&&p.first.second-(i==1?0:i)>s[p.second.first][p.second.second-i])
-            {
-                s[p.second.first][p.second.second-i]=p.first.second-(i==1?0:i);
-                q.push(make_pair(make_pair(p.first.first+1,p.first.second-(i==1?0:i)),make_pair(p.second.first,p.second.second-i)));
-            }
+            int t=v[x][y+1];
+            v[x][y+1]=v[x][y];
+            solve(x,y+1,coin+2,false);
+            v[x][y+1]=t;
         }
-        for(int i=1;i<=max(p.first.second,1);++i)
-        {
-            if(p.second.second+i==n+1)
-                break;
-            if(v[p.second.first][p.second.second+i]=='P'&&p.first.second-(i==1?0:i)>s[p.second.first][p.second.second+i])
-            {
-                s[p.second.first][p.second.second+i]=p.first.second-(i==1?0:i);
-                q.push(make_pair(make_pair(p.first.first+1,p.first.second-(i==1?0:i)),make_pair(p.second.first,p.second.second+i)));
-            }
-        }
-        q.pop();
-    }
-    cout<<"impossible\n";
+    return;
 }
 int main()
 {
-    cin>>m>>n>>d;
-    v.resize(m+2,vector<char>(n+2));
-    for(int i=1;i<=m;++i)
-        for(int j=1;j<=n;++j)
-            cin>>v[i][j];
-    solve();
+    cin>>m>>n;
+    v.resize(m+2,vector<int>(m+2,2));
+    l.resize(m+2,vector<int>(m+2,INT_MAX));
+    for(int x,y,i=0;i<n;++i)
+    {
+        cin>>x>>y;
+        cin>>v[x][y];
+    }
+    solve(1,1,0,true);
+    cout<<(l[m][m]==INT_MAX?-1:l[m][m])<<'\n';
     return 0;
 }
