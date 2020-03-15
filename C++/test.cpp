bdb18c2f29f1a5ee58da990545c3202779b76e1f
sheng <shengyu2006@hotmail.com>
2020-03-15 18:46:51 +0800

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,31 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,m,x,y;
-vector<vector<int> > v,ans;
-void solve(int a,int b,int c)
-{
-    if(ans[a][b]<=c||a<1||a>n||b<1||b>m)
-        return;
-    ans[a][b]=c;
-    solve(a-1,b,c+(v[a][b]==v[a-1][b]?0:1));
-    solve(a+1,b,c+(v[a][b]==v[a+1][b]?0:1));
-    solve(a,b-1,c+(v[a][b]==v[a][b-1]?0:1));
-    solve(a,b+1,c+(v[a][b]==v[a][b+1]?0:1));
-}
 int main()
 {
-    cin>>n>>m>>x>>y;
-    v.resize(n+2,vector<int>(m+2));
-    ans.resize(n+2,vector<int>(m+2,INT_MAX));
-    for(int i=1;i<=n;++i)
-        for(int j=1;j<=m;++j)
-            cin>>v[i][j];
-    solve(x,y,0);
-    for(int i=1;i<=n;++i)
+    int n,ans=0;
+    cin>>n;
+    list<pair<int,int> > d;
+    while(n--)
     {
-        for(int j=1;j<=m;++j)
-            cout<<ans[i][j]<<' ';
-        cout<<'\n';
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a==0)
+        {
+            d.push_back(make_pair(b,c));
+            ans+=c;
+        }
+        else
+        {
+            bool f=true;
+            for(list<pair<int,int> >::iterator i=d.begin();i!=d.end();)
+            {
+                if(i->second+45<c)
+                {
+                    list<pair<int,int> >::iterator t=i;
+                    ++i;
+                    d.erase(t);
+                }
+                else if(i->first>=b)
+                {
+                    f=false;
+                    d.erase(i);
+                    break;
+                }
+                else
+                    ++i;
+            }
+            if(f)
+                ans+=c;
+        }
     }
+    cout<<ans<<'\n';
     return 0;
 }
