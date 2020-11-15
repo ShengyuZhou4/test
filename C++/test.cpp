40028efc5779a3553196ee211d3af7d1d4b68ec3
sheng <shengyu2006@hotmail.com>
2020-11-15 13:45:19 +0800

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -2,16 +2,8 @@
 using namespace std;
 int main()
 {
-    int n;
-    cin>>n;
-    vector<pair<int,int> > a((1<<n)+1); 
-    for (int i = 1; i < a.size(); ++i)
-    {
-        cin>>a[i].first;
-        a[i].second=i;
-    }
-    sort(a.begin()+1,a.begin()+(1<<n)/2+1);
-    sort(a.begin()+(1<<n)/2+1,a.end());
-    cout<<(a[(1<<n)/2].first<a.back().first?a[(1<<n)/2].second:a.back().second)<<'\n';
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<'\n';
     return 0;
 }
