8204053612b837e1f49d752a70e27ffc3d11a698
E-duck <50363661+ShengyuZhou4@users.noreply.github.com>
2020-11-15 16:43:18 +0800

    Update test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,9 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+int c[100005],e[1000005];
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    cout<<a+b<<'\n';
-    return 0;
+	int n;
+	unsigned long long s=0;
+	cin>>n;
+	for (int i=1;i<=n;i++)
+	cin>>c[i];
+	for (int i=1;i<=n;i++)
+	cin>>e[i];
+	for (int i=1;i<=n;i++)
+	{
+		s+=c[i]*e[i]*2006/10086;
+		s=s%907282877488;
+	}
+	cout<<s<<endl;
+	return 0;
 }
