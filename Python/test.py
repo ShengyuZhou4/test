1d7f1fbc084e36310c81e8883612285e3e2fa129
E-duck <50363661+ShengyuZhou4@users.noreply.github.com>
2021-07-05 10:39:15 +0800

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,5 +1,17 @@
-a,b,c=map(int,input().split())
-if a+b>c and a+c>b and b+c>a:
-    print('YES')
-else:
-    print('NO')
+a1,a2,a3,a4,a5=map(int,input().split())
+a2+=a1//3
+a5+=a1//3
+a1=a1//3
+a1+=a2//3
+a3+=a2//3
+a2=a2//3
+a2+=a3//3
+a4+=a3//3
+a3=a3//3
+a3+=a4//3
+a5+=a4//3
+a4=a4//3
+a4+=a5//3
+a1+=a5//3
+a5=a5//3
+print(a1,a2,a3,a4,a5)
